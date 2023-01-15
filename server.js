e4f4316805de8dbea93e74e8fa971bf84b27c076
raphactn <raphael@leeksolucoes.com.br>
2023-01-15 19:55:17 -0300

    origin cors

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://instagram-downloader-kappa.vercel.app/",
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
