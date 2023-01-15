6eac4532d795c05a86fdacb2f57122a1a64fef3a
raphactn <raphael@leeksolucoes.com.br>
2023-01-15 20:03:50 -0300

    ajuste

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(
   cors({
     origin:
-      "https://instagram-downloader-kappa.vercel.app/instagramData" |
+      "https://instagram-downloader-kappa.vercel.app/instagramData" ||
       "https://instagram-downloader-kappa.vercel.app",
   })
 );
