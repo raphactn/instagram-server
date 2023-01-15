8b734516f227834993d99c78da84cd38136f5be0
raphactn <raphael@leeksolucoes.com.br>
2023-01-15 19:44:49 -0300

    first commit

---
+++ bot.js
@@ -0,0 +1,48 @@
+const puppeteer = require("puppeteer");
+
+const ListResultsServices = async ({ data }) => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: false,
+  });
+
+  let error = false;
+  let result = [];
+  let page;
+
+  page = await browser.newPage();
+  page.setCacheEnabled(false);
+
+  await page.goto("https://cors-anywhere.herokuapp.com");
+
+  const selector = await page.$(
+    "body > form > p:nth-child(2) > input[type=submit]:nth-child(1)"
+  );
+
+  if (selector !== null) {
+    await page.click(
+      "body > form > p:nth-child(2) > input[type=submit]:nth-child(1)",
+      { delay: 10, clickCount: 2 }
+    );
+  }
+
+  await page.goto(data);
+
+  await page
+    .waitForSelector("._aagv")
+    .then(() => {
+      return (error = false);
+    })
+    .catch(() => {
+      return (error = true);
+    });
+  result = await page.$$eval("._aagv > img", (images) =>
+    images.map((i) => i.src)
+  );
+
+  await browser.close();
+
+  return { result, error };
+};
+
+module.exports = ListResultsServices;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const ListResultsServices = require("./bot");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.post("/instagramData", async (req, res) => {
+  const { body } = req.body;
+  if (body) {
+    const result = await ListResultsServices({ data: body });
+    return res.send(result);
+  }
+  return res.status(400).send("Informe a url da imagem");
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+});
