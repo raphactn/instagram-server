dcb88ca5968cfce09f1c688406626e123cb635f8
raphactn <raphael@leeksolucoes.com.br>
2023-01-16 08:27:33 -0300

    ajuste cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: ['https://instagram-downloader-kappa.vercel.app', 'http://localhost:3000'],
   })
 );
 
