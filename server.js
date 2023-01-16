e6340caf0f8d2bc6cb7bb9fdc0f697e2eedfe71c
raphactn <raphael@leeksolucoes.com.br>
2023-01-15 21:34:38 -0300

    ajuste

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
 const express = require("express");
 const ListResultsServices = require("./bot");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://instagram-downloader-kappa.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "Content-Type",
-    "Authorization"
-  );
-  next();
-});
-
 app.post("/instagramData", async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Request-Width, Content-Type, Accept"
+  );
   const { body } = req.body;
   if (body) {
     const result = await ListResultsServices({ data: body });
