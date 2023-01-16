4885e2846dee287eb25590774c4ca3ca94e8dfa2
raphactn <raphael@leeksolucoes.com.br>
2023-01-15 21:30:10 -0300

    new tentative

--- server.js
+++ server.js
@@ -5,26 +5,24 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(
-  cors({
-    origin: [
-      "https://instagram-downloader-kappa.vercel.app/instagramData",
-      "https://instagram-downloader-kappa.vercel.app",
-      "https://instagramserverapi.herokuapp.com/",
-      "http://localhost:3000"
-    ],
-  })
-);
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
-    next(); 
-})
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://instagram-downloader-kappa.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
 
 app.post("/instagramData", async (req, res) => {
   const { body } = req.body;
