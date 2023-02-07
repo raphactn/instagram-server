a097b7d2de8a92e3510f363a6d10749b17a76e17
raphaelLeek <raphael@leeksolucoes.com.br>
2023-02-07 11:04:34 -0300

    server update

--- server.js
+++ server.js
@@ -7,35 +7,17 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: [
-      "https://instagram-downloader-kappa.vercel.app/",
-      "https://instagram-downloader-kappa.vercel.app",
-      "https://instagramserverapi.herokuapp.com/instagramData",
-      "http://localhost:3000",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Origin",
-      "X-Requested-With",
-      "Accept",
-      "x-client-key",
-      "x-client-token",
-      "x-client-secret",
-      "Authorization",
-    ],
-    credentials: true,
+    origin: "*",
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-
-app.post("/instagramData", async (req, res) => {
-  const { body } = req.body;
-  if (body) {
-    const result = await ListResultsServices({ data: body });
+app.get("/instagramData", async (req, res) => {
+  const { url } = req.query;
+  if (url) {
+    const result = await ListResultsServices({ data: url });
     return res.send(result);
   }
   return res.status(400).send("Informe a url da imagem");
