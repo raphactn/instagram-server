8beadb34f40d8851d9c855bf3344e2a9b20ae32c
raphaelLeek <raphael@leeksolucoes.com.br>
2023-02-07 11:16:39 -0300

    teste

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get("/instagramData", async (req, res) => {
   const { url } = req.query;
   if (url) {
